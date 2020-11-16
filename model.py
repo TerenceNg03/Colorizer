895dc76aad2f0b4407912f06369f60d3faabfb8f
STOAdamWu <stoicism03@gmail.com>
2020-11-16 10:23:42 +0800

    New model with less blur

--- model.py
+++ model.py
@@ -3,16 +3,19 @@ import torch.nn.functional as F
 import torch
 
 class Down(nn.Module):
-    def __init__(self, inc, out, pool = 2):
+    def __init__(self, inc, out, pool = 2, relu = True):
         super().__init__()
-        self.inc = inc
-        self.out = out
-        self.layer1 = nn.Sequential(nn.Conv2d(inc, (inc+out)//2, 3, padding = 1), nn.ReLU())
-        self.layer2 = nn.Sequential(nn.Conv2d((inc+out)//2, out, 3, padding = 1), nn.ReLU())
+        self.relu = relu
+        self.layer1 = nn.Sequential(nn.Conv2d(inc, (inc+out)//2, 3, padding = 1))
+        self.layer2 = nn.Sequential(nn.Conv2d((inc+out)//2, out, 3, padding = 1))
         self.pool = pool
     def forward(self, x):
         x = self.layer1(x)
+        if self.relu:
+            x = F.relu(x)
         x = self.layer2(x)
+        if self.relu:
+            x = F.relu(x)
         x = F.max_pool2d(x, self.pool)
         return x
     
@@ -40,10 +43,12 @@ class Colorizer(nn.Module):
         self.d4 = Down(128,256)
         self.u1 = Up(256,128)
         self.u2 = Up(256,64)
-        self.u3 = Up(128,8)
-        self.u4 = Up(16,3, relu = False)
+        self.u3 = Up(128,32)
+        self.u4 = Up(40,32)
+        self.out = Down(33,3,pool = 1, relu = False)
         
     def forward(self, x):
+        orig = x
         x = self.d1(x)
         out1 = x
         x = self.d2(x)
@@ -59,7 +64,8 @@ class Colorizer(nn.Module):
         x = self.u3(x)
         x = torch.cat([x,out1], dim = 1)
         x = self.u4(x)
-        
+        x = torch.cat([x,orig], dim = 1)
+        x = self.out(x)
         x = torch.sigmoid(x)
         return x
         
